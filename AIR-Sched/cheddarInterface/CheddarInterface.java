b2a1544360385cf4d1fcb4c98965709a3527b648
Candr3 <Candr3@gmail.com>
2014-05-15 19:05:15 +0100

    lol

--- AIR-Sched/cheddarInterface/CheddarInterface.java
+++ AIR-Sched/cheddarInterface/CheddarInterface.java
@@ -1,5 +1,48 @@
 package cheddarInterface;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import utils.BashUtils;
+
 public class CheddarInterface {
+
+	private static final String CHEDDAR_ROOT = "";
+	private static final String FILENAME = "litecheddar.sh";
+
+	public static boolean analyse() {
+
+		String[] cmd_array = { "chmod", "755", FILENAME };
+		BashUtils.cmdInterpreter(cmd_array);
+
+		return true;
+	}
+
+	public static boolean createBash() {
+		File f = new File(FILENAME);
+		if (f.exists()) {
+			f.delete();
+		}
+
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+
+			bw.write("#!/bin/bash\n");
+			bw.write("\n");
+			bw.write("export CHEDDAR_PATH=" + CHEDDAR_ROOT + "\n");
+			bw.write("export CHEDDAR_INSTALL_PATH=$CHEDDAR_PATH/graphical_editor/\n");
+			bw.write("$CHEDDAR_PATH/binaries/bin/release/cheddarlite\n");
+
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
 
 }
