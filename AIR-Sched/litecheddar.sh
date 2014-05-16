fb3bffb30bacdc6f624dac2952ec8edd68889509
Candr3 <Candr3@gmail.com>
2014-05-16 18:27:25 +0100

    fim do dia

---
+++ AIR-Sched/litecheddar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CHEDDAR_PATH=/home/andre/Cheddar/Cheddar_compil/terminal/src
+export CHEDDAR_INSTALL_PATH=$CHEDDAR_PATH/graphical_editor/
+$CHEDDAR_PATH/binaries/bin/release/cheddarlite $1 $2 $3 $4
